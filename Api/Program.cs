6ecc075bf15d635dc7f377000b401660593b99cc
kerildash <kerildash@gmail.com>
2024-02-13 17:52:32 +0300

    Configure app services

--- Api/Program.cs
+++ Api/Program.cs
@@ -2,8 +2,8 @@
 using Database.RepositoryInterfaces;
 using Database.Repositories;
 using Database;
-using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
+using Shared.Helper;
 
 namespace Api;
 
@@ -13,21 +13,35 @@ public class Program
 	{
 		var builder = WebApplication.CreateBuilder(args);
 
-		builder.Services.AddScoped<IBookRepository, BookRepository>();
-
+		AddRepositories(builder);
+		
 
 		builder.Services.AddControllers();
 		builder.Services.AddTransient<Seed>();
+		builder.Services.AddAutoMapper(typeof(Program).Assembly, typeof(MappingProfiles).Assembly);
 		// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 		builder.Services.AddEndpointsApiExplorer();
 		builder.Services.AddSwaggerGen();
-		builder.Services.AddDbContext<DataContext>(options =>
-		{
-			options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-		});
 
+		#region AddDbContext
+		builder.Services.AddDbContext<DataContext>(
+			options =>
+			options.UseSqlServer(
+				builder.Configuration.GetConnectionString("DefaultConnection"),
+				x => x.MigrationsAssembly(typeof(DataContext).Assembly.FullName)
+				)
+		);
+		#endregion
+
+		builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
 
 		var app = builder.Build();
+
+		# region seeding the data
+		if (args.Length == 1 && args[0].ToLower() == "seeddata")
+			SeedData(app);		
+		#endregion
+
 
 		// Configure the HTTP request pipeline.
 		if (app.Environment.IsDevelopment())
@@ -44,5 +58,22 @@ public class Program
 		app.MapControllers();
 
 		app.Run();
+	}
+
+	private static void AddRepositories(WebApplicationBuilder builder)
+	{
+		builder.Services.AddScoped<IBookRepository, BookRepository>();
+		builder.Services.AddScoped<IAuthorRepository, AuthorRepository>();
+	}
+
+	private static void SeedData(IHost app)
+	{
+		var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+		using (var scope = scopedFactory.CreateScope())
+		{
+			var service = scope.ServiceProvider.GetService<Seed>();
+			service.SeedDataContext();
+		}
 	}
 }
