c7e8729b9f4ea7d47c078a131f7edf6d9b970566
kerildash <kerildash@gmail.com>
2024-02-13 17:38:45 +0300

    Add post and get methods to the author and book controllers

---
+++ Api/Controllers/AuthorController.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using Database.RepositoryInterfaces;
+using Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Dto;
+
+namespace Api.Controllers;
+
+[Route("api/[controller]s")]
+[ApiController]
+public class AuthorController(IAuthorRepository repository, IMapper mapper) : ControllerBase
+{
+	[HttpGet]
+	[ProducesResponseType(200, Type = typeof(IEnumerable<AuthorDto>))]
+	[ProducesResponseType(400)]
+	public IActionResult Get()
+	{
+		var authors = mapper.Map<List<AuthorDto>>(repository.GetAll());
+		if (!ModelState.IsValid)
+		{
+			return BadRequest(ModelState);
+		}
+		return Ok(authors);
+	}
+	[HttpGet("id/{id}")]
+	[ProducesResponseType(200, Type = typeof(AuthorDto))]
+	[ProducesResponseType(400)]
+	public IActionResult Get(Guid id)
+	{
+		var author = mapper.Map<AuthorDto>(repository.Get(id));
+		if (!ModelState.IsValid)
+		{
+			return BadRequest(ModelState);
+		}
+		return Ok(author);
+	}
+	[HttpGet("by-book/{authorId}")]
+	[ProducesResponseType(200, Type = typeof(IEnumerable<AuthorDto>))]
+	[ProducesResponseType(400)]
+	public IActionResult GetByAuthorID(Guid authorId)
+	{
+		var authors = mapper.Map<List<AuthorDto>>(repository.GetByBookId(authorId));
+		if (!ModelState.IsValid)
+		{
+			return BadRequest(ModelState);
+		}
+		return Ok(authors);
+	}
+	[HttpPost]
+	[ProducesResponseType(204)]
+	[ProducesResponseType(400)]
+	public IActionResult Create([FromBody] AuthorDto authorCreate)
+	{
+		if (authorCreate is null)
+		{
+			return BadRequest(ModelState);
+		}
+		var authorMap = mapper.Map<Author>(authorCreate);
+		var isCreated = repository.Create(authorMap);
+		if (!isCreated)
+		{
+			ModelState.AddModelError("", "Error while saving");
+			return StatusCode(500, ModelState);
+		}
+		return Ok("Author created");
+	}
+}
---
+++ Api/Controllers/BookController.cs
@@ -0,0 +1,70 @@
+﻿using AutoMapper;
+using Database.RepositoryInterfaces;
+using Domain.Models;
+using Shared.Helper;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Dto;
+
+namespace Api.Controllers;
+
+[Route("api/[controller]s")]
+[ApiController]
+public class BookController(IBookRepository repository, IMapper mapper) : ControllerBase
+{
+	[HttpGet]
+	[ProducesResponseType(200, Type = typeof(IEnumerable<BookDto>))]
+	[ProducesResponseType(400)]
+	public IActionResult Get()
+	{
+		var books = mapper.Map<List<BookDto>>(repository.GetAll());
+		if (!ModelState.IsValid)
+		{
+			return BadRequest(ModelState);
+		}
+		return Ok(books);
+	}
+	[HttpGet("id/{id}")]
+	[ProducesResponseType(200, Type = typeof(BookDto))]
+	[ProducesResponseType(400)]
+	public IActionResult Get(Guid id)
+	{
+		var book = mapper.Map<BookDto>(repository.Get(id));
+		if (!ModelState.IsValid)
+		{
+			return BadRequest(ModelState);
+		}
+		return Ok(book);
+	}
+
+	[HttpGet("by-author/{authorId}")]
+	[ProducesResponseType(200, Type = typeof(IEnumerable<BookDto>))]
+	[ProducesResponseType(400)]
+	public IActionResult GetByAuthorID(Guid authorId)
+	{
+		var books = mapper.Map<List<BookDto>>(repository.GetByAuthorId(authorId));
+		if (!ModelState.IsValid)
+		{
+			return BadRequest(ModelState);
+		}
+		return Ok(books);
+	}
+
+	[HttpPost]
+	[ProducesResponseType(204)]
+	[ProducesResponseType(400)]
+	public IActionResult Create([FromQuery] List<Guid> authorIds,[FromBody] BookDto bookCreate)
+	{
+		if (bookCreate is null)
+		{
+			return BadRequest(ModelState);
+		}
+		var bookMap = mapper.Map<Book>(bookCreate);
+		var isCreated = repository.Create(authorIds, bookMap);
+		if (!isCreated)
+		{
+			ModelState.AddModelError("", "Error while saving");
+			return StatusCode(500, ModelState);
+		}
+		return Ok("Book created");
+	}
+}
