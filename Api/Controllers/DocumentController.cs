6178a25cb129061d505ed22a2c14a8248870fe82
kerildash <kerildash@gmail.com>
2024-03-19 18:00:40 +0300

    Add file upload and download

---
+++ Api/Controllers/DocumentController.cs
@@ -0,0 +1,71 @@
+﻿using AutoMapper;
+using Database.RepositoryInterfaces;
+using Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Dto;
+using System.Reflection.Metadata;
+using System.Text.RegularExpressions;
+
+namespace Api.Controllers
+{
+	[Route("api/[controller]s")]
+	[ApiController]
+	public class DocumentController(IDocumentRepository repository, IMapper mapper, IWebHostEnvironment environment) : ControllerBase
+	{
+		[HttpPost]
+		[ProducesResponseType(200)]
+		[ProducesResponseType(400)]
+		public async Task<IActionResult> Create( IFormFile documentCreate)
+		{
+			if (documentCreate is null)
+			{
+				return BadRequest(ModelState);
+			}
+			string name = HandleName(documentCreate.FileName);
+			string path = "/Files/" + name;
+			
+			using (var fileStream = new FileStream(environment.WebRootPath + path, FileMode.Create))
+			{
+				await documentCreate.CopyToAsync(fileStream);
+			}
+			var document = new Domain.Models.Document()
+			{
+				Id = Guid.NewGuid(),
+				Name = name,
+				Path = path
+			};
+
+			var isCreated = repository.Create(document);
+			if (!isCreated)
+			{
+				ModelState.AddModelError("", "Error while saving");
+				return StatusCode(500, ModelState);
+			}
+			return Ok(document.Id);
+		}
+		[HttpGet("id/{id}")]
+		public IActionResult Get(Guid id)
+		{
+			var document = repository.Get(id);
+			var fullPath = environment.WebRootPath + document.Path;
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+			return PhysicalFile(fullPath, "application/octet-stream", Path.GetFileName(fullPath));
+		}
+		private string HandleName(string name)
+		{
+			if (!Validate(name))
+			{
+				return $"{Guid.NewGuid()}.{System.IO.Path.GetExtension(name)}";
+			}
+			return Regex.Replace(name, @"\s+", "");
+		}
+		private bool Validate(string text, string pattern = @"^[a-zA-Z0-9](?:[a-zA-Z0-9 ._-]*[a-zA-Z0-9])?\.[a-zA-Z0-9_-]+$")
+		{
+			return Regex.IsMatch(text, pattern);
+		}
+	}
+}
---
+++ Database/Repositories/DocumentRepository.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Database.RepositoryInterfaces;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Database.Repositories
+{
+	public class DocumentRepository(DataContext context) : IDocumentRepository
+	{
+		public bool Create(Document document)
+		{
+			context.Add(document);
+			return Save();
+		}
+
+		public bool Delete(Guid id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool Exists(Guid id)
+		{
+			return context.Documents.Any(a => a.Id == id);
+		}
+
+		public Document Get(Guid id)
+		{
+			if (!Exists(id))
+			{
+				throw new ArgumentException("Document not found");
+			}
+			return context.Documents.FirstOrDefault(d => d.Id == id);
+		}
+
+		public ICollection<Document> Get(string name)
+		{
+			throw new NotImplementedException();
+		}
+
+		public ICollection<Document> GetAll()
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool Save()
+		{
+			try
+			{
+				return context.SaveChanges() > 0
+					? true
+					: throw new InvalidOperationException("Nothing to save");
+			}
+			catch
+			{
+				throw;
+			}
+		}
+
+		public bool Update(Document entity)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
