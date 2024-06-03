75b99bdbcb328b164a948b7f5b500cac0efb65a5
kerildash <kerildash@gmail.com>
2024-06-04 02:06:07 +0300

    Remove the body of SeedDataContext() because of its irrelevance.

--- Database/Seed.cs
+++ Database/Seed.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Domain.Models;
-
-namespace Database;
+﻿namespace Database;
 
 public class Seed
 {
@@ -16,39 +9,6 @@ public class Seed
 	}
 	public void SeedDataContext()
 	{
-		if (!_dataContext.BookAuthors.Any())
-		{
-			var bookAuthors = new List<BookAuthor>()
-			{
-				new BookAuthor()
-				{
-					Book = new Book()
-					{
-						Title = "Title",
-						Description = "Description"
-					},
-					Author = new Author()
-					{
-						Name = "Author",
-						Bio = "Bio"
-					}
-				},
-				new BookAuthor()
-				{
-					Book = new Book()
-					{
-						Title = "Title2",
-						Description = "Description2"
-					},
-					Author = new Author()
-					{
-						Name = "Author2",
-						Bio = "Bio2"
-					}
-				},
-			};
-			_dataContext.BookAuthors.AddRange(bookAuthors);
-			_dataContext.SaveChanges();
-		}
+		throw new NotImplementedException();
 	}
 }
