e9503fbe1d9e3b7dfdeb4b1c13356337d6c0835e
kerildash <kerildash@gmail.com>
2024-06-20 20:16:53 +0300

    Add TagService

---
+++ Api/Services/TagService.cs
@@ -0,0 +1,30 @@
+﻿using Api.Services.Interfaces;
+using Domain.Models;
+using System.Text.RegularExpressions;
+
+namespace Api.Services;
+
+public class TagService : ITagService
+{
+	public void HandleName(Tag tag)
+	{
+		if (!IsNameMatchThePrimaryFilter(tag))
+		{
+			throw new ArgumentException("Invalid tag name");
+		}
+		CorrectName(tag);
+	}
+	public bool IsNameCorrect(Tag tag)
+	{
+		return Regex.IsMatch(tag.Name, @"^[a-zа-яё0-9_]+$") || tag.Name.Length <= 30;
+	}
+
+	private bool IsNameMatchThePrimaryFilter(Tag tag)
+	{
+		return Regex.IsMatch(tag.Name, @"^[a-zA-Zа-яёА-ЯЁ0-9_]+$") || tag.Name.Length <= 30;
+	}
+	private void CorrectName(Tag tag) 
+	{
+		tag.Name = tag.Name.ToLower();
+	}
+}
