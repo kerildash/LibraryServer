9106e1f3278552adb57cf85f79c337f1b2a55c78
kerildash <kerildash@gmail.com>
2024-07-05 23:32:36 +0300

    Change resolvers: fix the type of repository parameter; change the name of the method called from the repository to match the new interface.

--- Shared/Resolvers/BookCoverResolver.cs
+++ Shared/Resolvers/BookCoverResolver.cs
@@ -5,11 +5,11 @@ using Shared.Dto.Domain;
 
 namespace Shared.Resolvers
 {
-	internal class BookCoverResolver(IPictureRepository repository) : IValueResolver<BookDto, Book, Picture>
+	internal class BookCoverResolver(IStaticFileRepository<Picture> repository) : IValueResolver<BookDto, Book, Picture>
 	{
 		public Picture Resolve(BookDto dto, Book book, Picture cover, ResolutionContext context)
 		{
-			return (Picture)repository.Get(dto.CoverId).Result;
+			return (Picture)repository.GetAsync(dto.CoverId).Result;
 		}
 	}
 }
--- Shared/Resolvers/BookDocumentResolver.cs
+++ Shared/Resolvers/BookDocumentResolver.cs
@@ -5,11 +5,11 @@ using Database.RepositoryInterfaces;
 
 namespace Shared.Resolvers
 {
-    internal class BookDocumentResolver(IDocumentRepository repository) : IValueResolver<BookDto, Book, Document>
+    internal class BookDocumentResolver(IStaticFileRepository<Document> repository) : IValueResolver<BookDto, Book, Document>
     {
         public Document Resolve(BookDto dto, Book book, Document document, ResolutionContext context)
         {
-            return (Document)repository.Get(dto.DocumentId).Result;
+            return (Document)repository.GetAsync(dto.DocumentId).Result;
         }
     }
 }
