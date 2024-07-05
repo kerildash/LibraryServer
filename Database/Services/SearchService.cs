10a3b0165ab4ead2a7625fb7fa8589f48b1c6dc8
kerildash <kerildash@gmail.com>
2024-07-05 23:28:25 +0300

    Add SearchService: an implementation of ISearchService

---
+++ Database/Services/SearchService.cs
@@ -0,0 +1,9 @@
+﻿namespace Database.Services;
+
+public class SearchService<T> : ISearchService<T>
+{
+	public Task<ICollection<T>> FindAsync(string query)
+	{
+		throw new NotImplementedException();
+	}
+}
