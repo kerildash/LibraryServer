e9513984aa866ec9636910240b65275ceb02ea7d
kerildash <kerildash@gmail.com>
2024-07-05 23:38:28 +0300

    Change restrictions for T in IStaticFileService

--- Api/Services/Interfaces/IStaticFileService.cs
+++ Api/Services/Interfaces/IStaticFileService.cs
@@ -2,9 +2,8 @@
 
 namespace Api.Services.Interfaces;
 
-public interface IStaticFileService<T> where T : StaticFile, new()
+public interface IStaticFileService<T> where T : StaticFile
 {
     internal Task<T> CreateAsync(IFormFile staticFileCreate);
     internal bool IsValidExtension(IFormFile file);
-
 }
